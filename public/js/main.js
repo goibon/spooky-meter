a31d7618c60bd2060ec4b99f36f9a73712c55f84
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2018-08-19 23:29:04 +0200

    Removed initializeUI function from main.js and moved its body to the outermost scope.

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,39 @@
-/* global $ */
+const spookyMeterImage = $('#spooky-meter').children('img')
+const spookyMeterImageSource = spookyMeterImage.attr('src')
+const title = $('title')
+const spookyDescriptions = ['Not Spooky - It is safe but always be alert for skeletons',
+  'Spoopy - No skeletons nearby but I\'m kind of scared anyway',
+  'P Spoopy - Imminent danger of skeletons',
+  'Spooky - There is a skeleton right behind you look out',
+  '2Spooky - It is too late a skeleton is inside you']
 
-function initializeUI () {
-  const spookyMeterImage = $('#spooky-meter').children('img')
-  const spookyMeterImageSource = spookyMeterImage.attr('src')
-  const title = $('title')
-  const spookyDescriptions = ['Not Spooky - It is safe but always be alert for skeletons',
-    'Spoopy - No skeletons nearby but I\'m kind of scared anyway',
-    'P Spoopy - Imminent danger of skeletons',
-    'Spooky - There is a skeleton right behind you look out',
-    '2Spooky - It is too late a skeleton is inside you']
+var spookyLevel = spookyMeterImageSource[spookyMeterImageSource.length - 5]
 
-  var spookyLevel = spookyMeterImageSource[spookyMeterImageSource.length - 5]
-
-  function randomizeSpookyLevel () {
-    spookyLevel = Math.floor(Math.random() * (4 - 0 + 1)) + 0
-  }
-
-  function updateSpookyMeterImage (src) {
-    if (src) {
-      spookyMeterImage.attr('src', src)
-      return
-    }
-    spookyMeterImage.attr('src', '/images/level' + spookyLevel + '.gif')
-  }
-
-  function updateTitle (text) {
-    if (text) {
-      title.html(text)
-      return
-    }
-    title.html(spookyDescriptions[spookyLevel])
-  }
-
-  window.PullToRefresh.init({
-    mainElement: 'body',
-    onRefresh: function () {
-      randomizeSpookyLevel()
-      updateSpookyMeterImage()
-      updateTitle()
-    }
-  })
+function randomizeSpookyLevel () {
+  spookyLevel = Math.floor(Math.random() * (4 - 0 + 1)) + 0
 }
 
-$(function () {
-  initializeUI()
+function updateSpookyMeterImage (src) {
+  if (src) {
+    spookyMeterImage.attr('src', src)
+    return
+  }
+  spookyMeterImage.attr('src', '/images/level' + spookyLevel + '.gif')
+}
+
+function updateTitle (text) {
+  if (text) {
+    title.html(text)
+    return
+  }
+  title.html(spookyDescriptions[spookyLevel])
+}
+
+window.PullToRefresh.init({
+  mainElement: 'body',
+  onRefresh: function () {
+    randomizeSpookyLevel()
+    updateSpookyMeterImage()
+    updateTitle()
+  }
 })
