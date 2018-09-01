683aff7578e4ac98ca0050a77f8b8ec0271a6694
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2018-09-01 17:25:34 +0200

    Converted the spookyMeter module to work as both a CommonJS module and an ES6 module.

--- public/js/spookyMeter.js
+++ public/js/spookyMeter.js
@@ -1,16 +1,28 @@
-const spookyDescriptions = [
-  'Not Spooky - It is safe but always be alert for skeletons',
-  'Spoopy - No skeletons nearby but I\'m kind of scared anyway',
-  'P Spoopy - Imminent danger of skeletons',
-  'Spooky - There is a skeleton right behind you look out',
-  '2Spooky - It is too late a skeleton is inside you'
-]
+(function () {
+  let spookyMeter = {}
 
-exports.getSpookyLevel = function () {
-  return Math.floor(Math.random() * spookyDescriptions.length)
-}
+  if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = spookyMeter
+  } else if (typeof exports !== 'undefined') {
+    exports.spookyMeter = spookyMeter
+  } else {
+    this.spookyMeter = spookyMeter
+  }
 
-exports.getSpookyDescription = function (spookyLevel) {
-  spookyLevel = Math.min(spookyLevel, spookyDescriptions.length - 1) || spookyDescriptions.length - 1
-  return spookyDescriptions[spookyLevel]
-}
+  const spookyDescriptions = [
+    'Not Spooky - It is safe but always be alert for skeletons',
+    'Spoopy - No skeletons nearby but I\'m kind of scared anyway',
+    'P Spoopy - Imminent danger of skeletons',
+    'Spooky - There is a skeleton right behind you look out',
+    '2Spooky - It is too late a skeleton is inside you'
+  ]
+
+  spookyMeter.getSpookyLevel = function () {
+    return Math.floor(Math.random() * spookyDescriptions.length)
+  }
+
+  spookyMeter.getSpookyDescription = function (spookyLevel) {
+    spookyLevel = Math.min(spookyLevel, spookyDescriptions.length - 1) || spookyDescriptions.length - 1
+    return spookyDescriptions[spookyLevel]
+  }
+}).call(this || window)
