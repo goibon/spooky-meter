b778bb672aa7c76637ceb302653bf76f3278e152
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2018-09-01 23:35:45 +0200

    Changed some strings from using single-quotes to using back ticks to utilize string interpolation and avoid escaping.

--- public/js/spookyMeter.js
+++ public/js/spookyMeter.js
@@ -11,7 +11,7 @@
 
   const spookyDescriptions = [
     'Not Spooky - It is safe but always be alert for skeletons',
-    'Spoopy - No skeletons nearby but I\'m kind of scared anyway',
+    `Spoopy - No skeletons nearby but I'm kind of scared anyway`,
     'P Spoopy - Imminent danger of skeletons',
     'Spooky - There is a skeleton right behind you look out',
     '2Spooky - It is too late a skeleton is inside you'
