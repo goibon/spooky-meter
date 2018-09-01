323c3e11e55f9e19eed139d3855da9cf80f15db4
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2018-09-01 12:58:24 +0200

    Formatted main.js so that properties and entries in objects and arrays are written on separate lines.

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ app.use(express.static(__dirname))
 app.use(express.static('public'))
 app.set('view engine', 'pug')
 
-const spookyDescriptions = ['Not Spooky - It is safe but always be alert for skeletons',
+const spookyDescriptions = [
+  'Not Spooky - It is safe but always be alert for skeletons',
   'Spoopy - No skeletons nearby but I\'m kind of scared anyway',
   'P Spoopy - Imminent danger of skeletons',
   'Spooky - There is a skeleton right behind you look out',
-  '2Spooky - It is too late a skeleton is inside you']
+  '2Spooky - It is too late a skeleton is inside you'
+]
 
 app.get('/', function (request, response) {
   var spookylevel = Math.floor(Math.random() * (4 - 0 + 1)) + 0
-  response.render('layouts/index', {spookylevel: spookylevel,
-    title: spookyDescriptions[spookylevel]})
+  response.render('layouts/index', {
+    spookylevel: spookylevel,
+    title: spookyDescriptions[spookylevel]
+  })
 })
 
 app.listen(8080, function () {
